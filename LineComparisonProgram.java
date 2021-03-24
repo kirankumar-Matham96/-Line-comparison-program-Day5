514d8417c21fc0c11c3351d5ac5f2997515ba7f0
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-24 17:42:14 +0530

    ADD: Checked if length of two lines is equal

--- LineComparisonProgram.java
+++ LineComparisonProgram.java
@@ -6,7 +6,8 @@ public class LineComparisonProgram
 	{
 		double length_of_line = 0;
 
-		if(x2 > x1 && y2 > y1) //For keeping the value positive inside 'sqrt' method
+		//For keeping the value positive inside 'sqrt' method
+		if(x2 > x1 && y2 > y1)
 		{
 			length_of_line = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		}
@@ -27,19 +28,57 @@ public class LineComparisonProgram
 
 	public static void main(String[] args)
 	{
+		//variables
+		double X1=0, Y1=0, X2=0, Y2=0, x1=0, y1=0, x2=0, y2=0;
+
 		//Taking the input from the user
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter X1 co-ordinate: ");
-		double X1 = sc.nextDouble();
-		System.out.println("Enter Y1 co-ordinate: ");
-		double Y1 = sc.nextDouble();
-		System.out.println("Enter X2 co-ordinate: ");
-		double X2 = sc.nextDouble();
-		System.out.println("Enter Y2 co-ordinate: ");
-		double Y2 = sc.nextDouble();
+		System.out.println("Enter the co-ordinates of first line end points");
+		for(int i=0;i<2;i++)
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter X1 co-ordinate: ");
+			double VAR_x1 = sc.nextDouble();
+			System.out.println("Enter Y1 co-ordinate: ");
+			double VAR_y1 = sc.nextDouble();
+			System.out.println("Enter X2 co-ordinate: ");
+			double VAR_x2 = sc.nextDouble();
+			System.out.println("Enter Y2 co-ordinate: ");
+			double VAR_y2 = sc.nextDouble();
 
-		double line_length = lineLength(X1,Y1,X2,Y2);
+			//To avoid repeating above statements
+			if(i==0)
+			{	//first line co-ordinates
+				X1 = VAR_x1;
+				Y1 = VAR_y1;
+				X2 = VAR_x2;
+				Y2 = VAR_y2;
+				System.out.println("Enter the co-ordinates of second line end points");
+			}
+			else
+			{	//second line co-ordinates
+				x1 = VAR_x1;
+				y1 = VAR_y1;
+				x2 = VAR_x2;
+				y2 = VAR_y2;
+			}
+		}
 
-		System.out.println("length: "+line_length);
+		//Calling the method and storing the returned value
+		double length_of_line_1 = lineLength(X1,Y1,X2,Y2);
+		double length_of_line_2 = lineLength(x1,y1,x2,y2);
+
+		//Creating objects for the variables
+		Double dl1 = length_of_line_1;
+		Double dl2 = length_of_line_2;
+
+		//checking equality condition
+		if(dl1.equals(dl2))
+		{
+			System.out.println("Two lines are equal in length");
+		}
+		else
+		{
+			System.out.println("Two lines are NOT equal in length");
+		}
 	}
 }
